c65ab68ef64b64a4ccba7e1361aa49c7241162aa
gunnpeeps <romger.fletcher@gmail.com>
2020-07-26 15:10:54 -0700

    Uses meetups.json file

---
+++ convert.js
@@ -0,0 +1,10 @@
+$(function () {
+
+  // Variables
+  var cards = $("#card-container");
+
+  // Create on website
+  $.getJSON("./meetups.json", function (meetups) {
+    cards.append(Mustache.render($("#meetup-card-template").html(), { "meetups": meetups }));
+  });
+});
