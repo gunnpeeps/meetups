0651e7728ae91dd79d2ec42fcdbbe9ec0503cf53
Om Mahesh <2om.mahesh@gmail.com>
2020-07-27 21:18:19 -0700

    Fixed small main.js thing

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,1 @@
 var database = new Database();
-async function bleh() { console.log(await database.getAllMeetups()); }
-
-bleh();
-//never
-
-i = 0;
-
-var ha = "yes";
-
-function Start() {
-  printf(ha)
-}
